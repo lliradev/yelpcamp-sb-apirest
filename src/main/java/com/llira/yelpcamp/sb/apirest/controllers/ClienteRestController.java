be95ac1b020ef4f29bc7bfd1f9593407ce7b343b
LLira34 <luislira1029@gmail.com>
2020-10-24 18:55:04 -0500

    Se agrego el método para subir una imagen

--- src/main/java/com/llira/yelpcamp/sb/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/llira/yelpcamp/sb/apirest/controllers/ClienteRestController.java
@@ -1,12 +1,7 @@
 package com.llira.yelpcamp.sb.apirest.controllers;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
+import com.llira.yelpcamp.sb.apirest.entity.Cliente;
+import com.llira.yelpcamp.sb.apirest.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
@@ -16,180 +11,197 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.llira.yelpcamp.sb.apirest.entity.Cliente;
-import com.llira.yelpcamp.sb.apirest.service.IClienteService;
+import javax.validation.Valid;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:8080" })
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
 
-	@Autowired
-	private IClienteService clienteService;
+    @Autowired
+    private IClienteService clienteService;
 
-	/**
-	 * Método para obtener una lista de registros sin paginación
-	 * 
-	 * @return {@link ResponseEntity}
-	 */
-	@GetMapping("/clientes")
-	public ResponseEntity<?> index() {
-		Map<String, Object> params = new HashMap<>();
-		List<Cliente> clientes;
-		try {
-			clientes = clienteService.findAll();
-		} catch (DataAccessException e) {
-			params.put("message", "Se produjo un error al consultar en la base de datos.");
-			params.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return new ResponseEntity<>(clientes, HttpStatus.OK);
-	}
+    /**
+     * Método para obtener una lista de registros sin paginación
+     *
+     * @return {@link ResponseEntity}
+     */
+    @GetMapping("/clientes")
+    public ResponseEntity<?> index() {
+        Map<String, Object> params = new HashMap<>();
+        List<Cliente> clientes;
+        try {
+            clientes = clienteService.findAll();
+        } catch (DataAccessException e) {
+            params.put("message", "Se produjo un error al consultar en la base de datos.");
+            params.put("error", e.getMessage() + ": " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
 
-	/**
-	 * Método para obtener una lista paginada de los registros
-	 * 
-	 * @param page    página actual
-	 * @param limit   limite de registros
-	 * @param orderBy campo a ordenar
-	 * @param shape   forma ascendente o descendente
-	 * @return {@link ResponseEntity}
-	 */
-	@GetMapping("/clientes/paginated")
-	public ResponseEntity<?> index(@RequestParam(name = "page", defaultValue = "0") Integer page,
-			@RequestParam(name = "limit", defaultValue = "5") Integer limit,
-			@RequestParam(name = "orderBy", defaultValue = "id") String orderBy,
-			@RequestParam(name = "shape", defaultValue = "desc") String shape) {
-		Map<String, Object> params = new HashMap<>();
-		Page<Cliente> clientes;
-		try {
-			Pageable pageable = PageRequest.of(page, limit, Sort.by(orderBy).descending());
-			if (shape.equals("asc"))
-				pageable = PageRequest.of(page, limit, Sort.by(orderBy));
-			System.out.println(pageable);
-			clientes = clienteService.findAll(pageable);
-		} catch (DataAccessException e) {
-			params.put("message", "Se produjo un error al consultar en la base de datos.");
-			params.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return new ResponseEntity<>(clientes, HttpStatus.OK);
-	}
+    /**
+     * Método para obtener una lista paginada de los registros
+     *
+     * @param page    página actual
+     * @param limit   limite de registros
+     * @param orderBy campo a ordenar
+     * @param shape   forma ascendente o descendente
+     * @return {@link ResponseEntity}
+     */
+    @GetMapping("/clientes/paginated")
+    public ResponseEntity<?> index(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                   @RequestParam(name = "limit", defaultValue = "5") Integer limit,
+                                   @RequestParam(name = "orderBy", defaultValue = "id") String orderBy,
+                                   @RequestParam(name = "shape", defaultValue = "desc") String shape) {
+        Map<String, Object> params = new HashMap<>();
+        Page<Cliente> clientes;
+        try {
+            Pageable pageable = PageRequest.of(page, limit, Sort.by(orderBy).descending());
+            if (shape.equals("asc"))
+                pageable = PageRequest.of(page, limit, Sort.by(orderBy));
+            clientes = clienteService.findAll(pageable);
+        } catch (DataAccessException e) {
+            params.put("message", "Se produjo un error al consultar en la base de datos.");
+            params.put("error", e.getMessage() + ": " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
 
-	/**
-	 * Método para obtener un registro en específico
-	 * 
-	 * @param id identificador único del registro
-	 * @return {@link ResponseEntity}
-	 */
-	@GetMapping("/clientes/{id}")
-	public ResponseEntity<?> show(@PathVariable Long id) {
-		Cliente cliente;
-		Map<String, Object> params = new HashMap<>();
-		try {
-			cliente = clienteService.findById(id);
-		} catch (DataAccessException e) {
-			params.put("message", "Se produjo un error al consultar en la base de datos.");
-			params.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		if (cliente == null) {
-			params.put("message", "No se encontro el cliente.");
-			return new ResponseEntity<>(params, HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
-	}
+    /**
+     * Método para obtener un registro en específico
+     *
+     * @param id identificador único del registro
+     * @return {@link ResponseEntity}
+     */
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Cliente cliente;
+        Map<String, Object> params = new HashMap<>();
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            params.put("message", "Se produjo un error al consultar en la base de datos.");
+            params.put("error", e.getMessage() + ": " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        if (cliente == null) {
+            params.put("message", "No se encontro el cliente.");
+            return new ResponseEntity<>(params, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
+    }
 
-	/**
-	 * Método para crear un nuevo objeto
-	 * 
-	 * @param cliente objeto a insertar en la base de datos
-	 * @return {@link ResponseEntity}
-	 */
-	@PostMapping("/clientes")
-	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
-		Map<String, Object> params = new HashMap<>();
-		if (result.hasErrors()) {
-			List<String> errors = result.getFieldErrors().stream()
-					.map(e -> "El campo [" + e.getField().toUpperCase() + "] " + e.getDefaultMessage())
-					.collect(Collectors.toList());
-			params.put("errors", errors);
-			params.put("message", "Los campos no cumplen con la validación.");
-			return new ResponseEntity<>(params, HttpStatus.BAD_REQUEST);
-		}
-		try {
-			clienteService.save(cliente);
-		} catch (DataAccessException e) {
-			params.put("message", "Se produjo un error al insertar en la base de datos.");
-			params.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return new ResponseEntity<>(null, HttpStatus.CREATED);
-	}
+    /**
+     * Método para crear un nuevo objeto
+     *
+     * @param cliente objeto a insertar en la base de datos
+     * @return {@link ResponseEntity}
+     */
+    @PostMapping("/clientes")
+    public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
+        Map<String, Object> params = new HashMap<>();
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors().stream()
+                    .map(e -> "El campo [" + e.getField().toUpperCase() + "] " + e.getDefaultMessage())
+                    .collect(Collectors.toList());
+            params.put("errors", errors);
+            params.put("message", "Los campos no cumplen con la validación.");
+            return new ResponseEntity<>(params, HttpStatus.BAD_REQUEST);
+        }
+        try {
+            clienteService.save(cliente);
+        } catch (DataAccessException e) {
+            params.put("message", "Se produjo un error al insertar en la base de datos.");
+            params.put("error", e.getMessage() + ": " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
 
-	/**
-	 * Método para editar un objeto
-	 * 
-	 * @param cliente objeto a editar
-	 * @param id      identificador único del registro a editar
-	 * @return {@link ResponseEntity}
-	 */
-	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
-		Cliente c1 = clienteService.findById(id);
-		Map<String, Object> params = new HashMap<>();
-		if (result.hasErrors()) {
-			List<String> errors = result.getFieldErrors().stream()
-					.map(e -> "El campo [" + e.getField().toUpperCase() + "] " + e.getDefaultMessage())
-					.collect(Collectors.toList());
-			params.put("errors", errors);
-			return new ResponseEntity<>(params, HttpStatus.BAD_REQUEST);
-		}
-		if (c1 == null) {
-			params.put("message", "No se encontro el cliente.");
-			return new ResponseEntity<>(params, HttpStatus.NOT_FOUND);
-		}
-		try {
-			c1.setApellido(cliente.getApellido());
-			c1.setNombre(cliente.getNombre());
-			c1.setEmail(cliente.getEmail());
-			c1.setCreatedAt(cliente.getCreatedAt());
-			clienteService.save(c1);
-		} catch (DataAccessException e) {
-			params.put("message", "Se produjo un error al editar en la base de datos.");
-			params.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return new ResponseEntity<>(null, HttpStatus.CREATED);
-	}
+    /**
+     * Método para editar un objeto
+     *
+     * @param cliente objeto a editar
+     * @param id      identificador único del registro a editar
+     * @return {@link ResponseEntity}
+     */
+    @PutMapping("/clientes/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente, BindingResult result, @PathVariable Long id) {
+        Cliente c = clienteService.findById(id);
+        Map<String, Object> params = new HashMap<>();
+        if (result.hasErrors()) {
+            List<String> errors = result.getFieldErrors().stream()
+                    .map(e -> "El campo [" + e.getField().toUpperCase() + "] " + e.getDefaultMessage())
+                    .collect(Collectors.toList());
+            params.put("errors", errors);
+            return new ResponseEntity<>(params, HttpStatus.BAD_REQUEST);
+        }
+        if (c == null) {
+            params.put("message", "No se encontro el cliente.");
+            return new ResponseEntity<>(params, HttpStatus.NOT_FOUND);
+        }
+        try {
+            c.setApellido(cliente.getApellido());
+            c.setNombre(cliente.getNombre());
+            c.setEmail(cliente.getEmail());
+            c.setCreatedAt(cliente.getCreatedAt());
+            clienteService.save(c);
+        } catch (DataAccessException e) {
+            params.put("message", "Se produjo un error al editar en la base de datos.");
+            params.put("error", e.getMessage() + ": " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
 
-	/**
-	 * Método para eliminar un registro
-	 * 
-	 * @param id identificador único del registro a eliminar
-	 * @return {@link ResponseEntity}
-	 */
-	@DeleteMapping("/clientes/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id) {
-		Map<String, Object> params = new HashMap<>();
-		try {
-			clienteService.delete(id);
-		} catch (DataAccessException e) {
-			params.put("message", "Se produjo un error al eliminar en la base de datos.");
-			params.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
-			return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
-	}
+    /**
+     * Método para eliminar un registro
+     *
+     * @param id identificador único del registro a eliminar
+     * @return {@link ResponseEntity}
+     */
+    @DeleteMapping("/clientes/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        Map<String, Object> params = new HashMap<>();
+        try {
+            clienteService.delete(id);
+        } catch (DataAccessException e) {
+            params.put("message", "Se produjo un error al eliminar en la base de datos.");
+            params.put("error", e.getMessage() + ": " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(params, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    /**
+     * Método para subir una imagen
+     *
+     * @param image imagen del cliente
+     * @param id    identificador único del registro
+     * @return {@link ResponseEntity}
+     */
+    @PostMapping("/clientes/upload")
+    public ResponseEntity<?> upload(@RequestParam("image") MultipartFile image, @RequestParam("id") Long id) {
+        Map<String, Object> params = new HashMap<>();
+        Cliente cliente = clienteService.findById(id);
+        if (!image.isEmpty()) {
+            String nombre = image.getOriginalFilename();
+            Path ruta = Paths.get("uploads").resolve(nombre).toAbsolutePath();
+
+            // Files.copy(image.getInputStream(), ruta);
+        }
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/llira/yelpcamp/sb/apirest/entity/Cliente.java
+++ src/main/java/com/llira/yelpcamp/sb/apirest/entity/Cliente.java
@@ -1,91 +1,91 @@
 package com.llira.yelpcamp.sb.apirest.entity;
 
-import java.io.Serializable;
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "clientes")
 public class Cliente implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotEmpty(message = "no debe estar vacío.")
-	@Size(min = 4, max = 12, message = "debe tener entre 4 y 12 caracteres.")
-	@Column(nullable = false)
-	private String nombre;
+    @NotEmpty(message = "no debe estar vacío.")
+    @Size(min = 4, max = 12, message = "debe tener entre 4 y 12 caracteres.")
+    @Column(nullable = false)
+    private String nombre;
 
-	@NotEmpty(message = "no debe estar vacío.")
-	private String apellido;
+    @NotEmpty(message = "no debe estar vacío.")
+    private String apellido;
 
-	@NotEmpty(message = "no debe estar vacío.")
-	@Email(message = "debe ser una dirección de correo electrónico con formato correcto.")
-	@Column(nullable = false, unique = true)
-	private String email;
+    @NotEmpty(message = "no debe estar vacío.")
+    @Email(message = "debe ser una dirección de correo electrónico con formato correcto.")
+    @Column(nullable = false, unique = true)
+    private String email;
 
-	@Column(name = "created_at")
-	@Temporal(TemporalType.DATE)
-	private Date createdAt;
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
 
-	@PrePersist
-	public void prePersist() {
-		createdAt = new Date();
-	}
+    private String image;
 
-	public Long getId() {
-		return id;
-	}
+    @PrePersist
+    public void prePersist() {
+        createdAt = new Date();
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public String getApellido() {
-		return apellido;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+    public String getApellido() {
+        return apellido;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public Date getCreatedAt() {
-		return createdAt;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
+    public Date getCreatedAt() {
+        return createdAt;
+    }
 
-	private static final long serialVersionUID = 1L;
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    private static final long serialVersionUID = 1L;
 }
